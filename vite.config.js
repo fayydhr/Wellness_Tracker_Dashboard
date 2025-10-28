893b853b38e700c8278757757677777b4d9ca42a
fayydhr <141895647+fayydhr@users.noreply.github.com>
2025-10-28 09:55:00 +0700

    Remove lodash alias _createCaseFirst, clean vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '_createCaseFirst': 'lodash/startCase', 
-    }
-  },
-  optimizeDeps: {
-    include: ['lodash'],
-  },
-  build: {
-    commonjsOptions: {
-      include: [/node_modules/],
-    }
-  }
+  base: './',
 });
